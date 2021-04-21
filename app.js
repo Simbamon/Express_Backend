f78c1cdff693e4d9745f17fb9d1b92ddcb951a0f
Simbamon <sbhan9511@gmail.com>
2021-04-21 21:06:38 +0900

    Added fetch get name

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 var request = require('request')
+const router = express.Router()
 const app = express()
 const port = 5000
 const path = require('path')
+const fetch = require('node-fetch')
+
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 const apikey = process.env.API_KEY;
@@ -30,6 +33,26 @@ app.get("/getWeatherMinneapolis", (req, res) => {
         }
     )
 })
+
+app.get("/getname", async (req, res) => {
+  console.log("/getbit endpoint called")
+  const url = 'https://api.agify.io?name=meelad'
+  const options = {
+    "method": "GET",
+  }
+  const response = await fetch(url, options)
+    .then(res => res.json())
+    .catch(e => {
+      console.error({
+        "message": "Error",
+        error: e,
+      })
+    })
+  console.log("RESPONSE: ", response)
+  res.send(response.name)
+})
+
+console.log 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
